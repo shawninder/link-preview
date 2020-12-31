b1016588120f56aeb40715b6be52d30bbf9ed4d3
Shawn Inder <shawninder@gmail.com>
2020-12-30 20:18:48 -0500

    💄 Slightly improve event date formatting

--- eventDateExtractor.js
+++ eventDateExtractor.js
@@ -1,7 +1,12 @@
 const { $jsonld, toRule } = require('@metascraper/helpers')
+const moment = require('moment')
 
 module.exports = exports = function eventDateExtractor (opts) {
-  const toEventDate = toRule(i => i, opts)
+  const toEventDate = toRule((str) => {
+    const eventStart = moment(str)
+    const dateString = `${eventStart.format('YYYY-MM-DD HH:mm:ss')}`
+    return dateString
+  }, opts)
 
   return {
     eventDate: [
